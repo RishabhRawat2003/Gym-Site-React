c0f03713335ff614a022d4e9857cb0a7978749cb
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-05-30 13:01:17 +0530

    set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-green-500 text-center'>Rishabh</h1>
+    </>
+  )
+}
+
+export default App
